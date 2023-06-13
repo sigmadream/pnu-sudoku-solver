024564ed8103c235024f38371aaca6b502e3b26d
Sangkon, Han <sigmadream@gmail.com>
2023-06-14 02:07:11 +0900

    스도쿠 문제 해결 알고리즘 적용

--- src/sudoku_solver/main.py
+++ src/sudoku_solver/main.py
@@ -27,8 +27,44 @@ def print_sudoku(board):
                 else:
                     print(str(board[i][j]) + " ", end="")
 
-def solve():
-    return "Hello, Sudoku-Solver!"
+
+def is_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def is_valid(board, num, pos):
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+    boardx_x = pos[1] // 3
+    boardx_y = pos[0] // 3
+    for i in range(boardx_y*3, boardx_y*3 + 3):
+        for j in range(boardx_x * 3, boardx_x*3 + 3):
+            if board[i][j] == num and (i,j) != pos:
+                return False
+    return True
+
+def solve(board):
+    find = is_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+    for i in range(1,10):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+            print(f"\nSolution {row, col}: \n")
+            print_sudoku(board)
+            if solve(board):
+                return True
+            board[row][col] = 0
+    return False
 
 
 if __name__ == "__main__":
@@ -37,4 +73,4 @@ if __name__ == "__main__":
     board = make_board(problem)
     print("Problem:")
     print_sudoku(board)
-    print(solve())
+    solve(board)
